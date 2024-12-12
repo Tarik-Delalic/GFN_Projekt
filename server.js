b973c38fe3c4c6122eff32af6007a54b8d406e9e
Tarik Delalic <48967705+dela777@users.noreply.github.com>
2024-12-12 09:40:43 +0100

    Changes
    Changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
 const PORT = 3000;
 
 app.use(express.json());
@@ -17,3 +18,5 @@ app.use('/', apiRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
